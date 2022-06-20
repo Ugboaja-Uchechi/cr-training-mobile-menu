b9a8221057b3626899144cf28b4f405f69fd2179
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-06-20 23:10:14 +0100

    Add close icon, adjust styling and write event listener

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const menuBars = document.querySelector('.fa-bars');
+const menuBars = document.querySelector('.menu-bars');
+const close = document.querySelector('#close');
 const nav = document.querySelector('#desktop-nav');
+const logo = document.querySelector('#logo');
 
 menuBars.addEventListener('click', () => {
     nav.style.display = 'block'
     menuBars.style.display = 'none'
-  console.log('click')
+    close.style.display = 'block'
+})
+
+close.addEventListener(click, () => {
+  nav.style.display = 'none'
+  menuBars.style.display = 'block'
+  logo.style.display = 'block'
 })
