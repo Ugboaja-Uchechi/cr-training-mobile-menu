f8ef8225098aae43ab3dc0f7faedb7d6c23172e6
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-06-22 09:09:34 +0100

    Add eslint linter and fix linter errors

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const menuBars = document.querySelector('.menu-bars');
 const close = document.querySelector('#close');
 const nav = document.querySelector('#desktop-nav');
-const logo = document.querySelector('#logo');
+// const logo = document.querySelector('#logo');
 
 menuBars.addEventListener('click', () => {
-    nav.style.display = 'block'
-    menuBars.style.display = 'none'
-    close.style.display = 'block'
-})
+  nav.style.display = 'block';
+  menuBars.style.display = 'none';
+  close.style.display = 'block';
+});
 
-close.addEventListener(click, () => {
-  nav.style.display = 'none'
-  menuBars.style.display = 'block'
-  logo.style.display = 'block'
-})
+// close.addEventListener(click, () => {
+//   nav.style.display = 'none';
+//   menuBars.style.display = 'block';
+//   logo.style.display = 'block';
+// });
