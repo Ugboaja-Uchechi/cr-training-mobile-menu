e9f6482d3c8b8187b3931017de852f93acfd351d
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-06-23 09:39:51 +0100

    Fix code bug and remove commented code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuBars = document.querySelector('.menu-bars');
 const close = document.querySelector('#close');
 const nav = document.querySelector('#desktop-nav');
-// const logo = document.querySelector('#logo');
+const logo = document.querySelector('#logo');
 
 menuBars.addEventListener('click', () => {
   nav.style.display = 'block';
@@ -9,8 +9,9 @@ menuBars.addEventListener('click', () => {
   close.style.display = 'block';
 });
 
-// close.addEventListener(click, () => {
-//   nav.style.display = 'none';
-//   menuBars.style.display = 'block';
-//   logo.style.display = 'block';
-// });
+close.addEventListener('click', () => {
+  nav.style.display = 'none';
+  menuBars.style.display = 'block';
+  logo.style.display = 'block';
+  close.style.display = 'none';
+});
